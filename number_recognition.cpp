966374e00c83efd9c973dfa15c043faa0625dfcb
lsy0727 <92630416+lsy0727@users.noreply.github.com>
2024-06-05 15:57:05 +0900

    Update number_recognition.cpp

--- number_recognition.cpp
+++ number_recognition.cpp
@@ -5,13 +5,15 @@ using namespace cv;
 
 Mat img(500, 900, CV_8UC3, Scalar(255, 255, 255));	//3채널 컬러영상
 Mat img_size(500 / 5, 200, CV_8UC3, Scalar(255, 255, 255));
-Mat contour_img;
-Mat gray, bin, rev;
+Mat draw_img;
+Mat gray, bin;
 Point ptOld;
 string file_name;
 
 void on_mouse(int event, int x, int y, int flags, void*);	//마우스 이벤트
 void img_UI(Mat& img);	//영상 UI 그리기 함수
+Mat bounding_img(Mat img);	//바운딩박스
+Mat morph(Mat img);	//모폴로지 연산
 
 int main() {
 	img_UI(img);	//UI그리기 함수 호출
@@ -27,17 +29,17 @@ int main() {
 void on_mouse(int event, int x, int y, int flags, void*) {
 	imshow("img", img);
 	Rect rect_area[] = {
-		Rect(0, 0, 500, 500),	//입력창 영역
-		Rect(501, 0, 199, 99),	//save 영역
-		Rect(501, 500 / 5 + 1, 199, 99),	//load 영역
-		Rect(501, 500 * 2 / 5 + 1, 199, 99),	//clear 영역
-		Rect(501, 500 * 3 / 5 + 1, 199, 99),	//run 영역
-		Rect(501, 500 * 4 / 5 + 1, 199, 99),	//exit 영역
-		Rect(700, 0, 199, 99),	//contour 영역
-		Rect(700, 500 / 5 + 1, 199, 99),
-		Rect(700, 500 * 2 / 5 + 1, 199, 99),
-		Rect(700, 500 * 3 / 5 + 1, 199, 99),
-		Rect(700, 500 * 4 / 5 + 1, 199, 99)
+		Rect(0, 0, 500, 500),	//0. 입력창 영역
+		Rect(501, 0, 199, 99),	//1. save 영역
+		Rect(501, 500 / 5 + 1, 199, 99),	//2. load 영역
+		Rect(501, 500 * 2 / 5 + 1, 199, 99),	//3. clear 영역
+		Rect(501, 500 * 3 / 5 + 1, 199, 99),	//4. run 영역
+		Rect(501, 500 * 4 / 5 + 1, 199, 99),	//5. exit 영역
+		Rect(700, 0, 199, 99),	//6. contour 영역
+		Rect(700, 500 / 5 + 1, 199, 99),	//7. center 영역
+		Rect(700, 500 * 2 / 5 + 1, 199, 99),	//8.
+		Rect(700, 500 * 3 / 5 + 1, 199, 99),	//9.
+		Rect(700, 500 * 4 / 5 + 1, 199, 99)	//10.
 	};
 	switch (event) {
 	case EVENT_LBUTTONDOWN:
@@ -72,13 +74,9 @@ void on_mouse(int event, int x, int y, int flags, void*) {
 		else if (rect_area[6].contains(Point(x, y))) {	//contour
 			cout << "contour press" << endl;
 
-			contour_img = img(Rect(0, 0, 500, 500));
-			cvtColor(contour_img, gray, COLOR_BGR2GRAY);
-			threshold(gray, bin, 0, 255, THRESH_BINARY | THRESH_OTSU);
-			rev = ~bin;
+			bin = morph(img);
 			vector<vector<Point>> contours;
-			findContours(rev, contours, RETR_LIST, CHAIN_APPROX_NONE);
-
+			findContours(bin, contours, RETR_LIST, CHAIN_APPROX_NONE);
 			cout << "외곽선 갯수 : " << contours.size() << endl;
 			if (contours.size() == 1) {
 				cout << "예상 결과 : 1, 2, 3, 5, 7" << endl;
@@ -89,6 +87,23 @@ void on_mouse(int event, int x, int y, int flags, void*) {
 			else if (contours.size() == 3) {
 				cout << "예상 결과 : 8" << endl;
 			}
+			imshow("contour", bin);
+		}
+		else if (rect_area[7].contains(Point(x, y))) {	//무게중심
+			cout << "center press" << endl;
+
+			Mat bin = bounding_img(img);
+
+			Mat labels, stats, centroids;
+			int cnt = connectedComponentsWithStats(bin, labels, stats, centroids);
+			int width = stats.at<int>(1, 2);	//width
+			int height = stats.at<int>(1, 3);	//height
+			int center_x = centroids.at<double>(1, 0);	//무게중심 x좌표
+			int center_y = centroids.at<double>(1, 1);	//무게중심 y좌표
+			int per_x = (double)center_x / (width + center_x) * 100;
+			int per_y = (double)center_y / (height + center_y) * 100;
+			cout << per_x << "%" << endl << per_y << "%" << endl;
+			imshow("bounding box", bin);
 		}
 		break;
 	case EVENT_MOUSEMOVE:
@@ -100,6 +115,36 @@ void on_mouse(int event, int x, int y, int flags, void*) {
 		}
 		break;
 	}
+}
+Mat bounding_img(Mat img) {	//바운딩 박스
+	Mat bin = morph(img);
+
+	Mat labels, stats, centroids;
+	int cnt = connectedComponentsWithStats(bin, labels, stats, centroids);
+
+	int* p = stats.ptr<int>(1);
+	
+	bin = bin(Rect(p[0], p[1], p[2], p[3]));
+	return bin;
+}
+Mat morph(Mat img) {	//모폴로지 연산
+	Mat bin;
+	draw_img = img(Rect(0, 0, 500, 500));
+	cvtColor(draw_img, gray, COLOR_BGR2GRAY);
+	threshold(gray, bin, 0, 255, THRESH_BINARY_INV | THRESH_OTSU);
+
+	Mat labels, stats, centroids;
+	int cnt = connectedComponentsWithStats(bin, labels, stats, centroids);
+	int morph_size = 10;
+	if (cnt > 2) {
+		while (true) {
+			morphologyEx(bin, bin, MORPH_CLOSE, Mat(morph_size, morph_size, CV_8UC1));
+			cnt = connectedComponentsWithStats(bin, labels, stats, centroids);
+			if (cnt <= 2) break;
+			morph_size += 3;
+		}
+	}
+	return bin;
 }
 
 void img_UI(Mat& img) {
@@ -113,7 +158,7 @@ void img_UI(Mat& img) {
 
 	//UI설계
 	vector<vector<string>> text = { {"Save", "Load", "Clear", "Run", "Exit"},
-		{"contour", "feature2", "feature3", "feature4", "feafure5"} };
+		{"contour", "center", "feature3", "feature4", "feafure5"} };
 	int fontface = FONT_HERSHEY_SIMPLEX;	//폰트 종류
 	double fontscale = 1.0;	//폰트 크기
 	int thickness = 2;	//글씨 두께
